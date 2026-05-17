6700895ab03605f8081a909ea8131af42b116d15
Mathar2007 <matharf172007@gmail.com>
2026-05-17 15:01:50 +0000

    Latest Code

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -129,6 +129,51 @@ public class Tic_Tac_Toe {
         return bestMove;
     }
 
+    static char[] doToss(){
+        char humanSymbol , aiSymbol , humanFirst ; 
+        System.out.println("\nTossing.......");
+        if(Math.random() < 0.5){
+            humanFirst = 'Y';
+            System.out.println("You won the toss! ");
+            String choice;
+// Asking from the human which symbol he want.
+            do{
+                System.out.print("\nChoose your symbol (X/O) : ");
+                choice = sc.next().toUpperCase();
+                if(! choice.equals("X") && ! choice.equals("O")){
+                    System.out.println("Invalid Choice! Enter X or O only.");
+                }
+            }while(! choice.equals("X") && ! choice.equals("O"));
+
+            if(choice.equals("X")){
+                humanSymbol = 'X';
+                aiSymbol = 'O';
+                System.out.println("You Choose The Symbol \" "+humanSymbol+" \" AI Symbol \" "+aiSymbol+" \"");
+            }
+            else{
+                humanSymbol = 'O';
+                aiSymbol = 'X';
+                System.out.println("You Choose The Symbol \" "+humanSymbol+" \" AI Symbol \" "+aiSymbol+" \"");
+            }
+        }
+        else{
+            humanFirst = 'N';
+            System.out.println("AI won the toss!");
+            if(Math.random() < 0.5){
+                aiSymbol = 'X';
+                humanSymbol = 'O';
+                System.out.println("AI Choose The Symbol \" "+aiSymbol+" \" Your Symbol \" "+humanSymbol+" \"");
+            }
+            else{
+                aiSymbol = 'O';
+                humanSymbol = 'X';
+                System.out.println("AI Choose The Symbol \" "+aiSymbol+" \" Your Symbol \" "+humanSymbol+" \"");
+            }
+        }
+        char[] result = {humanSymbol, aiSymbol , humanFirst};
+        return result;  
+    }
+
     static char [][] board = new char[3][3];
     
     public static void main(String[] args){
@@ -142,48 +187,76 @@ public class Tic_Tac_Toe {
                 board[i][j] = '_';
             }
         }
+// Tossing.
+        char [] tossResult = doToss();
+        char humanSymbol = tossResult[0];
+        char aiSymbol = tossResult[1];
+        boolean humanFirst = (tossResult[2] == 'Y');
 // Printing the board
-         printBoard(board);
-
-        do{
-            //    Human Turn
-// Asking the row and column from the Player 1 to place 'X' at specific value.
-            System.out.println("\nYour Turn!");
-            System.out.print("\nEnter Row (1-3) : ");
-            int row  = sc.nextInt();
-            System.out.print("Enter Column (1-3) : ");
-            int column = sc.nextInt();
-
-// Input Validation.
-            if(!(row>=1 && row<=3 && column>=1 && column<=3)){
-                System.out.println("Invalid Choice! Try Again. ");
-                continue;
-            }
+        printBoard(board);
+        if(humanFirst){
+            do{
+                //  Human Turn First
+// Asking the row and column from the Player 1 to place human symbol at specific value.
+                System.out.println("\nYour Turn!");
+                System.out.print("\nEnter Row (1-3) : ");
+                int row  = sc.nextInt();
+                System.out.print("Enter Column (1-3) : ");
+                int column = sc.nextInt();
+// InputValidation.
+                if(!(row>=1 && row<=3 && column>=1 && column<=3)){
+                    System.out.println("Invalid Choice! Try Again. ");
+                    continue;
+                }
 // Checking the cell is empty or not.
-            if(checkEmptyCells(board, row-1, column-1)){
-// Assigning the Player 1 symbol 'X' at the specific place.
-                board [row-1][column-1] = 'X';
-                System.out.println("After Your Turn.");
-                printBoard(board);
+                if(checkEmptyCells(board, row-1, column-1)){
+// Assigning the Player 1 symbol at the specific place.
+                    board [row-1][column-1] = humanSymbol;
+                    System.out.println("After Your Turn.");
+                    printBoard(board);
 // Checking that Player 1 wins or not.
-                if(checkWin(board,  'X')){
-                    System.out.println("\nYou Win!");
-                    break;
-                }
+                    if(checkWin(board, humanSymbol)){
+                        System.out.println("\nYou Win!");
+                        break;
+                    }
 // Checking that the game is draw or not.
-                if(isDraw(board)){
-                    System.out.println("\nDraw!");
-                    break;
+                    if(isDraw(board)){
+                        System.out.println("\nDraw!");
+                        break;
+                    }
+                    // AI Turn  
+                    int [] aiMove = getBestMove(board);
+// Assigning the AI symbol at specific place.
+                    board[aiMove[0]][aiMove[1]] = aiSymbol;
+                    System.out.println("\nAi Turn!\n");
+                    printBoard(board);
+// Checking that AI wins or not.
+                    if(checkWin(board, aiSymbol)){
+                        System.out.println("\nAI Wins!");
+                        break;
+                    }
+// Checking that game is draw or not.
+                    if(isDraw(board)){
+                        System.out.println("\nDraw!");
+                        break;
+                    }
                 }
-
-                // AI Turn  
+                else{
+                    System.out.print("\nCell is Already Occupied! Try Again.");
+                }
+            }while(true);
+        }
+        else{
+            boolean round = true;
+            do{
+                // AI Turn First
                 int [] aiMove = getBestMove(board);
-// Assigning the AI symbol 'O' at specific place.
-                board[aiMove[0]][aiMove[1]] = 'O';
+// Assigning the AI symbol at specific place.
                 System.out.println("\nAi Turn!\n");
+                board[aiMove[0]][aiMove[1]] = aiSymbol;
                 printBoard(board);
 // Checking that AI wins or not.
-                if(checkWin(board, 'O')){
+                if(checkWin(board, aiSymbol)){
                     System.out.println("\nAI Wins!");
                     break;
                 }
@@ -192,11 +265,45 @@ public class Tic_Tac_Toe {
                     System.out.println("\nDraw!");
                     break;
                 }
-            }
-            else{
-                System.out.print("\nCell is Already Occupied! Try Again.");
-            }
-        }while(true);
+                // Human Turn
+                do{
+// Asking the row and column from the Player 1 to place human symbol at specific value.
+                    System.out.println("\nYour Turn!");
+                    System.out.print("\nEnter Row (1-3) : ");
+                    int row  = sc.nextInt();
+                    System.out.print("Enter Column (1-3) : ");
+                    int column = sc.nextInt();
+// InputValidation.
+                    if(!(row>=1 && row<=3 && column>=1 && column<=3)){
+                        System.out.println("Invalid Choice! Try Again. ");
+                        continue;
+                    }
+// Checking the cell is empty or not.
+                    if(checkEmptyCells(board, row-1, column-1)){
+// Assigning the Player 1 symbol at the specific place.
+                        board [row-1][column-1] = humanSymbol;
+                        System.out.println("After Your Turn.");
+                        printBoard(board);
+// Checking that Player 1 wins or not.
+                        if(checkWin(board, humanSymbol)){
+                            System.out.println("\nYou Win!");
+                            round = false;
+                            break;
+                        }
+// Checking that the game is draw or not.
+                        if(isDraw(board)){
+                            System.out.println("\nDraw!");
+                            round = false;
+                            break;
+                        }
+                        break;
+                    }
+                    else{
+                        System.out.println("\nCell is Already Occupied! Try Again.");
+                    }
+                }while(true);
+            }while(round);
+        }
 // Asking from the user that he/she want to play again or not.
         do{
             System.out.print("\nDo you want to play again (y/n) : ");
